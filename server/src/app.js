2c8fcc04a50f3e404cdd968a303f92bdb215ae3b
Nick Bennett <nrbennett129@gmail.com>
2018-05-14 10:15:07 -0500

    Changed GET request to POST

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/register', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
     message: 'Your user was registered! Have fun!'
   })

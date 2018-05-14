378cca995da8292b523e7fbb597a597ba1ca0878
Nick Bennett <nrbennett129@gmail.com>
2018-05-14 10:13:43 -0500

    Added /register endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.get('/register', (req, res) => {
   res.send({
-    message: 'Hello, World!'
+    message: 'Your user was registered! Have fun!'
   })
 })
 

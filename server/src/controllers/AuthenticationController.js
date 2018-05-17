faebf0d11f3cdfbf389b8d15cdbb56947eb7e904
Nick Bennett <nrbennett129@gmail.com>
2018-05-17 13:02:18 -0500

    Implemented login endpoint for server

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -10,5 +10,36 @@ module.exports = {
         error: 'This email account is already in use.'
       })
     }
+  },
+
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+      if (!user) {
+        return res.send(403).send({
+          error: `${email} is not a registered email address.`
+        })
+      }
+      const isPasswordValid = password === user.password
+      if (!isPasswordValid) {
+        return res.send(403).send({
+          error: 'Invalid password.'
+        })
+      }
+
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson
+      })
+    } catch (error) {
+      res.status(500).send({
+        error: 'An error has occured trying to log in.'
+      })
+    }
   }
 }
